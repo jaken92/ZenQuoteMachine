cd78d52aef66b41dbe871aa2d3127694e21fa47e
jaken92 <jaaakeeen@gmail.com>
2023-05-17 15:52:49 +0200

    initial upload

---
+++ zen-project/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
+
+function App() {
+  return (
+    <>
+      <h1>Hello yung zen</h1>
+    </>
+  );
+}
+
+export default App;
