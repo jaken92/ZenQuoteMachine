e6ceea93293addc620318431e722689510bdfb2d
jaken92 <jaaakeeen@gmail.com>
2023-05-18 16:33:54 +0200

    Fetch from APIninjas using api key set in env.local

--- zen-project/src/App.tsx
+++ zen-project/src/App.tsx
@@ -5,6 +5,7 @@ import {
   QueryClientProvider,
   useQuery,
 } from '@tanstack/react-query';
+const key = import.meta.env.VITE_SOME_KEY;
 
 const queryClient = new QueryClient();
 
@@ -19,24 +20,29 @@ export default function App() {
 
 function Example() {
   const { isLoading, error, data } = useQuery({
-    queryKey: ['repoData'],
+    queryKey: ['quotes'],
     queryFn: () =>
-      fetch('https://api.github.com/repos/tannerlinsley/react-query').then(
-        (res) => res.json()
-      ),
+      fetch('https://api.api-ninjas.com/v1/quotes', {
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': key,
+        },
+      }).then((res) => res.json()),
   });
 
   if (isLoading) return <div>'Loading...'</div>;
 
   if (error) return <div>'An error has occurred: '</div>;
 
+  console.log(data);
+
   return (
     <div>
-      <h1>{data.name}</h1>
+      {/* <h1>{data.name}</h1>
       <p>{data.description}</p>
       <strong>👀 {data.subscribers_count}</strong>{' '}
       <strong>✨ {data.stargazers_count}</strong>{' '}
-      <strong>🍴 {data.forks_count}</strong>
+      <strong>🍴 {data.forks_count}</strong> */}
     </div>
   );
 }
