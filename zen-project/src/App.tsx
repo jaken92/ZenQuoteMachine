76ac69a011856a58b6203bb335938032d1fd50ac
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-05-18 12:47:34 +0200

    Update App.tsx
    Added TanStack query test example

--- zen-project/src/App.tsx
+++ zen-project/src/App.tsx
@@ -6,12 +6,37 @@ import {
   useQuery,
 } from '@tanstack/react-query';
 
-function App() {
+const queryClient = new QueryClient();
+
+export default function App() {
   return (
-    <>
-      <h1>Hello yung zen</h1>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <div>Hello Tanstack!</div>
+      <Example />
+    </QueryClientProvider>
   );
 }
 
-export default App;
+function Example() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch('https://api.github.com/repos/tannerlinsley/react-query').then(
+        (res) => res.json()
+      ),
+  });
+
+  if (isLoading) return <div>'Loading...'</div>;
+
+  if (error) return <div>'An error has occurred: '</div>;
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
+  );
+}
