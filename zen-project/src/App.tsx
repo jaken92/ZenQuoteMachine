e40e9401307f9d97a3514186e2c84626aa64e5df
alun0511 <78014508+alun0511@users.noreply.github.com>
2023-05-23 13:28:39 +0200

    testing fetch with several query keys

--- zen-project/src/App.tsx
+++ zen-project/src/App.tsx
@@ -31,7 +31,8 @@ export default function App() {
   return (
     <>
       <div>Hello Tanstack!</div>
-      <RandomQuote />
+      <RandomQuote/>
+      {/* <TestQuote/> */}
     </>
   );
 }
@@ -40,7 +41,7 @@ function RandomQuote() {
   const { isLoading, error, data } = useQuery({
     queryKey: ['quotes'],
     queryFn: () =>
-      fetch('https://api.api-ninjas.com/v1/quotes', {
+      fetch('https://api.api-ninjas.com/v1/quotes/', {
         headers: {
           'Content-Type': 'application/json',
           'x-api-key': key,
@@ -55,7 +56,6 @@ function RandomQuote() {
   if (error) return <div>'An error has occurred: '</div>;
 
   if (!data) return null;
-  console.log(data);
 
   return (
     <div>
@@ -64,5 +64,33 @@ function RandomQuote() {
       <h2>Category: "{data[0].category}"</h2>
     </div>
   );
-}
 
+}
+// function TestQuote({category:string, limit:number}) {
+//   const { isLoading, error, data } = useQuery({
+//     queryKey: ['quotes', category, limit],
+//     queryFn: () =>
+//       fetch('https://api.api-ninjas.com/v1/quotes?category=' + category + '&limit=' + limit, {
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'x-api-key': key,
+//         },
+//       })
+//         .then((res) => res.json())
+//         .then((data) => data as MyArray),
+//   });
+
+//   if (isLoading) return <div>'Loading...'</div>;
+
+//   if (error) return <div>'An error has occurred: '</div>;
+
+//   if (!data) return null;
+
+//   return (
+//     <div>
+//       <h1>"{data[0].quote}"</h1>
+//       <h2>Author: "{data[0].author}"</h2>
+//       <h2>Category: "{data[0].category}"</h2>
+//     </div>
+//   );
+// }
