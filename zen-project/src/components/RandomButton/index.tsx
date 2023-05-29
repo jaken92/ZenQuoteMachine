04bec60ddeabcdfe3c6044d070592ec8ca949c0a
jaken92 <jaaakeeen@gmail.com>
2023-05-29 14:55:22 +0200

    styled randombtn started

--- zen-project/src/components/RandomButton/index.tsx
+++ zen-project/src/components/RandomButton/index.tsx
@@ -1,4 +1,5 @@
 import { RandomButtonProps } from '../../utils/types';
+import styled from 'styled-components';
 
 export const RandomButton: React.FC<RandomButtonProps> = ({
   category,
@@ -6,21 +7,34 @@ export const RandomButton: React.FC<RandomButtonProps> = ({
   btnText,
 }) => {
   return (
-    <button value={category} onClick={() => clickFunction(category)}>
-      {btnText}
-    </button>
+    <div>
+      <StyledRandomBtn value={category} onClick={() => clickFunction(category)}>
+        {btnText}
+      </StyledRandomBtn>
+    </div>
   );
 };
 
-// const Button = (props: { category: string; limit?: number }) => {
-//   const { category, limit } = props;
-//   const upperCaseCategory =
-//     category.charAt(0).toUpperCase() + category.slice(1);
-//   return (
-//     <button
-//       onClick={() => console.log('Category: ', category, 'Limit: ', limit)}
-//     >
-//       {upperCaseCategory}
-//     </button>
-//   );
-// };
+const StyledRandomBtn = styled.button`
+  background: linear-gradient(-45deg, #c86344, #e73c7e, #23a6d5, #04cc29);
+  background-size: 300% 300%;
+  animation: gradient 5s ease infinite;
+  cursor: pointer;
+  margin: 0 auto;
+
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+
+  .StyledRandomBtn:hover {
+    background: black;
+  }
+`;
