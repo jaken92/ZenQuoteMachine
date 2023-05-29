6cf4a512f54653ead27886a592b39794db9a3d94
alun0511 <78014508+alun0511@users.noreply.github.com>
2023-05-29 11:02:54 +0200

    added refetch and removed quote component

--- zen-project/src/App.tsx
+++ zen-project/src/App.tsx
@@ -1,26 +1,70 @@
 import React from 'react';
 import './App.css';
-import { Button, Limiter, Quote } from './components';
+import { Button, Limiter} from './components';
+import { QuoteType } from './utils/types';
+import { useQuery } from '@tanstack/react-query';
 import categories from './utils/categories';
 
 export default function App() {
+
   const [category, setCategory] = React.useState<string>('');
   const [limit, setLimit] = React.useState<number>(1);
+
+
+  const key: string = import.meta.env.VITE_API_KEY;
+  
+  function Quote(props: { category: string; limit?: number }) {
+  //  Function to test passing of props as states. Returns mapped PropsArray.
+
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ['quotes', props.category],
+    queryFn: () =>
+      fetch(
+        'https://api.api-ninjas.com/v1/quotes?category=' +
+          props.category +
+          '&limit=' +
+          props.limit,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': key,
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => data as QuoteType[]),
+        refetchOnWindowFocus: false,
+    });
+
+  
+
+  if (isLoading) return <div>Loading ...</div>;
+
+  if (error instanceof Error) return <div>An error has occurred: </div>;
+
+  if (!data) return <div>No data found.</div>;
+
+
+  return (
+    <div>
+      {data.map((item: QuoteType, index: number) => (
+
+        <div key={index}>
+          <p>Quote: "{item.quote}"</p>
+          <p>Author: "{item.author}"</p>
+          <p>Category: "{item.category.charAt(0).toUpperCase() + item.category.slice(1)}"</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 
   //  Default values of the states category and limit are set within the last paranthesis.
 
   const quoteCategories: string[] = categories;
 
-  // const initialCategory: string = "";
-
-  // const resetCategory = () => {
-  //   setCategory(initialCategory);
-  // }
-
   const handleBtnClick = (category: string) => {
-    // setGetNew(false);
-    // resetCategory;
-
     setCategory(category);
   };
 
@@ -28,6 +72,8 @@ export default function App() {
   function handleLimitCallback(val: number) {
     setLimit(val);
   }
+
+
 
   return (
     <>
--- zen-project/src/components/index.ts
+++ zen-project/src/components/index.ts
@@ -1,3 +1,2 @@
 export * from "./Button";
 export * from "./Limiter";
-export * from "./Quote";
