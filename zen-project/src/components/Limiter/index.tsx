6bf7f53daf88147e0bcb27649406c35f493d44aa
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-05-30 14:24:20 +0200

    Updated radio button layout.

--- zen-project/src/components/Limiter/index.tsx
+++ zen-project/src/components/Limiter/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledLimiter = styled.div`
+  display: flex;
+  flex-direction: column;
+  .radio-buttons {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
 
 // I won't lie. I'm not at all sure what Typscript does here (arg0, WTF?), but here we add the function "limtiCallback" as a prop, thus making it possible to send the current value in the drop-down menu back to App.tsx, via the handleLimitCallback-function in App.tsx:
-
 export const Limiter = (props: { limitCallback: (arg0: number) => void }) => {
   // This will set the number simultaneous fetched qoutes to 1, 3 or 5.
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -11,17 +20,40 @@ export const Limiter = (props: { limitCallback: (arg0: number) => void }) => {
 
   // Returning 3 radio buttons with hard-coded values:
   return (
-    <div>
-      <label>
-        Show me this many quotes:
-        <input type="radio" name="myRadio" value="1" onChange={handleChange} />1
-      </label>
-      <label>
-        <input type="radio" name="myRadio" value="3" onChange={handleChange} />3
-      </label>
-      <label>
-        <input type="radio" name="myRadio" value="5" onChange={handleChange} />5
-      </label>
-    </div>
+    <StyledLimiter>
+      <h4>Show me this many quotes:</h4>
+      <div className="radio-buttons">
+        <label>
+          <input
+            className="radio-button"
+            type="radio"
+            name="myRadio"
+            value="1"
+            onChange={handleChange}
+          />
+          1
+        </label>
+        <label>
+          <input
+            className="radio-button"
+            type="radio"
+            name="myRadio"
+            value="3"
+            onChange={handleChange}
+          />
+          3
+        </label>
+        <label>
+          <input
+            className="radio-button"
+            type="radio"
+            name="myRadio"
+            value="5"
+            onChange={handleChange}
+          />
+          5
+        </label>
+      </div>
+    </StyledLimiter>
   );
 };
